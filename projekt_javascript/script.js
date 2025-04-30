fe534d1d9ecbe9de13de170feba486b07a0c3173
Jachym Kahanec <jachym.kahanec@centrum.cz>
2025-04-30 18:01:30 +0200

    Add files via upload

---
+++ projekt_javascript/script.js
@@ -0,0 +1,28 @@
+/**
+ * Responzivní webová stránka.
+ *
+ * @author Jáchym Kahanec
+ * @version 1.0
+ */
+
+/**Funkce rozbaluje hamburger menu*/
+
+const burgerMenuOpening = () => {
+  const menuIcon = document.querySelector(".menu-icon");
+  const menuList = document.querySelector("nav");
+  const hamburgerIcon = document.querySelector(".fa-solid");
+
+  menuIcon.addEventListener("click", () => {
+    if (hamburgerIcon.classList[1] === "fa-bars") {
+      hamburgerIcon.classList.remove("fa-bars");
+      hamburgerIcon.classList.add("fa-xmark");
+      menuList.style.display = "block";
+    } else {
+      hamburgerIcon.classList.remove("fa-xmark");
+      hamburgerIcon.classList.add("fa-bars");
+      menuList.style.display = "none";
+    }
+  });
+};
+
+burgerMenuOpening();
